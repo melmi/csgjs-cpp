cf0ff4196635404acbacd18434b8906836203b7c
Mohammad Elmi <elmi.moh@gmail.com>
2013-11-17 16:39:01 +0330

    Casing changed.

--- csgjs.cpp
+++ csgjs.cpp
@@ -8,35 +8,35 @@
 namespace csgjs
 {
 
-// `CSG.Plane.EPSILON` is the tolerance used by `splitPolygon()` to decide if a
+// `CSG.Plane.EPSILON` is the tolerance used by `split_polygon()` to decide if a
 // point is on the plane.
 static const double EPSILON = 0.00001;
 
-struct polygon;
+struct Polygon;
 
 // Represents a plane in 3D space.
-struct plane
+struct Plane
 {
-	vector normal;
+	Vector normal;
 	double w;
 
-	plane();
-	plane(const vector & a, const vector & b, const vector & c);
+	Plane();
+	Plane(const Vector & a, const Vector & b, const Vector & c);
 	bool ok() const;
 	void flip();
-	void splitPolygon(const polygon & _polygon, std::vector<polygon> & coplanarFront, std::vector<polygon> & coplanarBack, std::vector<polygon> & front, std::vector<polygon> & back) const;
+	void split_polygon(const Polygon & polygon, std::vector<Polygon> & coplanarFront, std::vector<Polygon> & coplanarBack, std::vector<Polygon> & front, std::vector<Polygon> & back) const;
 };
 
 // Represents a convex polygon. The vertices used to initialize a polygon must
 // be coplanar and form a convex loop. 
-struct polygon
+struct Polygon
 {
-	std::vector<vertex> vertices;
-	plane _plane;
+	std::vector<Vertex> vertices;
+	Plane plane;
 	void flip();
 
-	polygon();
-	polygon(const std::vector<vertex> & list);
+	Polygon();
+	Polygon(const std::vector<Vertex> & list);
 };
 
 // Holds a node in a BSP tree. A BSP tree is built from a collection of polygons
@@ -44,68 +44,68 @@ struct polygon
 // polygons) are added directly to that node and the other polygons are added to
 // the front and/or back subtrees. This is not a leafy BSP tree since there is
 // no distinction between internal and leaf nodes.
-struct csgnode
+struct Node
 {
-	std::vector<polygon> polygons;
-	csgnode * front;
-	csgnode * back;
-	plane _plane;
+	std::vector<Polygon> polygons;
+	Node * front;
+	Node * back;
+	Plane plane;
 
-	csgnode();
-	csgnode(const std::vector<polygon> & list);
-	~csgnode();
+	Node();
+	Node(const std::vector<Polygon> & list);
+	~Node();
 
-	csgnode * clone() const;
-	void clipTo(const csgnode * other);
+	Node * clone() const;
+	void clip_to(const Node * other);
 	void invert();
-	void build(const std::vector<polygon> & polygon);
-	std::vector<polygon> clipPolygons(const std::vector<polygon> & list) const;
-	std::vector<polygon> allPolygons() const;
+	void build(const std::vector<Polygon> & polygon);
+	std::vector<Polygon> clip_polygons(const std::vector<Polygon> & list) const;
+	std::vector<Polygon> all_polygons() const;
 };
 
 // Vector implementation
 
-inline static vector operator + (const vector & a, const vector & b) { return vector(a.x + b.x, a.y + b.y, a.z + b.z); }
-inline static vector operator - (const vector & a, const vector & b) { return vector(a.x - b.x, a.y - b.y, a.z - b.z); }
-inline static vector operator * (const vector & a, double b) { return vector(a.x * b, a.y * b, a.z * b); }
-inline static vector operator / (const vector & a, double b) { return a * (1.0f / b); }
-inline static double dot(const vector & a, const vector & b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
-inline static vector lerp(const vector & a, const vector & b, double v) { return a + (b - a) * v; }
-inline static vector negate(const vector & a) { return a * -1.0f; }
-inline static double length(const vector & a) { return sqrt(dot(a, a)); }
-inline static vector unit(const vector & a) { return a / length(a); }
-inline static vector cross(const vector & a, const vector & b) { return vector(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x); }
+inline static Vector operator + (const Vector & a, const Vector & b) { return Vector(a.x + b.x, a.y + b.y, a.z + b.z); }
+inline static Vector operator - (const Vector & a, const Vector & b) { return Vector(a.x - b.x, a.y - b.y, a.z - b.z); }
+inline static Vector operator * (const Vector & a, double b) { return Vector(a.x * b, a.y * b, a.z * b); }
+inline static Vector operator / (const Vector & a, double b) { return a * (1.0f / b); }
+inline static double dot(const Vector & a, const Vector & b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+inline static Vector lerp(const Vector & a, const Vector & b, double v) { return a + (b - a) * v; }
+inline static Vector negate(const Vector & a) { return a * -1.0f; }
+inline static double length(const Vector & a) { return sqrt(dot(a, a)); }
+inline static Vector unit(const Vector & a) { return a / length(a); }
+inline static Vector cross(const Vector & a, const Vector & b) { return Vector(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x); }
 
 // Vertex implementation
 
 // Create a new vertex between this vertex and `other` by linearly
 // interpolating all properties using a parameter of `t`. Subclasses should
 // override this to interpolate additional properties.
-inline static vertex interpolate(const vertex & a, const vertex & b, double t)
+inline static Vertex interpolate(const Vertex & a, const Vertex & b, double t)
 {
-	vertex ret;
+	Vertex ret;
 	ret.pos = lerp(a.pos, b.pos, t);
 	return ret;
 }
 
 // Plane implementation
 
-plane::plane() : normal(), w(0.0f) 
+Plane::Plane() : normal(), w(0.0f) 
 {
 }
 
-bool plane::ok() const 
+bool Plane::ok() const 
 {
 	return length(this->normal) > 0.0f; 
 }
 
-void plane::flip()
+void Plane::flip()
 {
 	this->normal = negate(this->normal); 
 	this->w *= -1.0f;
 }
 
-plane::plane(const vector & a, const vector & b, const vector & c)
+Plane::Plane(const Vector & a, const Vector & b, const Vector & c)
 {
 	this->normal = unit(cross(b - a, c - a));
 	this->w = dot(this->normal, a);
@@ -116,7 +116,7 @@ plane::plane(const vector & a, const vector & b, const vector & c)
 // `coplanarFront` or `coplanarBack` depending on their orientation with
 // respect to this plane. Polygons in front or in back of this plane go into
 // either `front` or `back`.
-void plane::splitPolygon(const polygon & _polygon, std::vector<polygon> & coplanarFront, std::vector<polygon> & coplanarBack, std::vector<polygon> & front, std::vector<polygon> & back) const
+void Plane::split_polygon(const Polygon & polygon, std::vector<Polygon> & coplanarFront, std::vector<Polygon> & coplanarBack, std::vector<Polygon> & front, std::vector<Polygon> & back) const
 {
 	enum
 	{
@@ -128,58 +128,58 @@ void plane::splitPolygon(const polygon & _polygon, std::vector<polygon> & coplan
 
 	// Classify each point as well as the entire polygon into one of the above
 	// four classes.
-	int polygonType = 0;
+	int polygon_type = 0;
 	std::vector<int> types;
 
-	for (size_t i = 0; i < _polygon.vertices.size(); i++) 
+	for (size_t i = 0; i < polygon.vertices.size(); i++) 
 	{
-		double t = dot(this->normal, _polygon.vertices[i].pos) - this->w;
+		double t = dot(this->normal, polygon.vertices[i].pos) - this->w;
 		int type = (t < -EPSILON) ? BACK : ((t > EPSILON) ? FRONT : COPLANAR);
-		polygonType |= type;
+		polygon_type |= type;
 		types.push_back(type);
 	}
 
 	// Put the polygon in the correct list, splitting it when necessary.
-	switch (polygonType) 
+	switch (polygon_type) 
 	{
 	case COPLANAR:
 		{
-			if (dot(this->normal, _polygon._plane.normal) > 0)
-				coplanarFront.push_back(_polygon);
+			if (dot(this->normal, polygon.plane.normal) > 0)
+				coplanarFront.push_back(polygon);
 			else 
-				coplanarBack.push_back(_polygon);
+				coplanarBack.push_back(polygon);
 			break;
 		}
 	case FRONT:
 		{
-			front.push_back(_polygon);
+			front.push_back(polygon);
 			break;
 		}
 	case BACK:
 		{
-			back.push_back(_polygon);
+			back.push_back(polygon);
 			break;
 		}
 	case SPANNING:
 		{
-			std::vector<vertex> f, b;
-			for (size_t i = 0; i < _polygon.vertices.size(); i++) 
+			std::vector<Vertex> f, b;
+			for (size_t i = 0; i < polygon.vertices.size(); i++) 
 			{
-				int j = (i + 1) % _polygon.vertices.size();
+				int j = (i + 1) % polygon.vertices.size();
 				int ti = types[i], tj = types[j];
-				vertex vi = _polygon.vertices[i], vj = _polygon.vertices[j];
+				Vertex vi = polygon.vertices[i], vj = polygon.vertices[j];
 				if (ti != BACK) f.push_back(vi);
 				if (ti != FRONT) b.push_back(vi);
 				if ((ti | tj) == SPANNING) 
 				{
 					double t = (this->w - dot(this->normal, vi.pos)) / dot(this->normal, vj.pos - vi.pos);
-					vertex v = interpolate(vi, vj, t);
+					Vertex v = interpolate(vi, vj, t);
 					f.push_back(v);
 					b.push_back(v);
 				}
 			}
-			if (f.size() >= 3) front.push_back(polygon(f));
-			if (b.size() >= 3) back.push_back(polygon(b));
+			if (f.size() >= 3) front.push_back(Polygon(f));
+			if (b.size() >= 3) back.push_back(Polygon(b));
 			break;
 		}
 	}
@@ -187,17 +187,17 @@ void plane::splitPolygon(const polygon & _polygon, std::vector<polygon> & coplan
 
 // Polygon implementation
 
-void polygon::flip()
+void Polygon::flip()
 {
 	std::reverse(vertices.begin(), vertices.end());
-	_plane.flip();
+	plane.flip();
 }
 
-polygon::polygon()
+Polygon::Polygon()
 {
 }
 
-polygon::polygon(const std::vector<vertex> & list) : vertices(list), _plane(vertices[0].pos, vertices[1].pos, vertices[2].pos)
+Polygon::Polygon(const std::vector<Vertex> & list) : vertices(list), plane(vertices[0].pos, vertices[1].pos, vertices[2].pos)
 {
 }
 
@@ -205,17 +205,17 @@ polygon::polygon(const std::vector<vertex> & list) : vertices(list), _plane(vert
 
 // Return a new CSG solid representing space in either this solid or in the
 // solid `csg`. Neither this solid nor the solid `csg` are modified.
-inline static csgnode * csg_union(const csgnode * a1, const csgnode * b1)
+inline static Node * csg_union(const Node * a1, const Node * b1)
 {
-	csgnode * a = a1->clone();
-	csgnode * b = b1->clone();
-	a->clipTo(b);
-	b->clipTo(a);
+	Node * a = a1->clone();
+	Node * b = b1->clone();
+	a->clip_to(b);
+	b->clip_to(a);
 	b->invert();
-	b->clipTo(a);
+	b->clip_to(a);
 	b->invert();
-	a->build(b->allPolygons());
-	csgnode * ret = new csgnode(a->allPolygons());
+	a->build(b->all_polygons());
+	Node * ret = new Node(a->all_polygons());
 	delete a; a = 0;
 	delete b; b = 0;
 	return ret;
@@ -223,19 +223,19 @@ inline static csgnode * csg_union(const csgnode * a1, const csgnode * b1)
 
 // Return a new CSG solid representing space in this solid but not in the
 // solid `csg`. Neither this solid nor the solid `csg` are modified.
-inline static csgnode * csg_subtract(const csgnode * a1, const csgnode * b1)
+inline static Node * csg_subtract(const Node * a1, const Node * b1)
 {
-	csgnode * a = a1->clone();
-	csgnode * b = b1->clone();
+	Node * a = a1->clone();
+	Node * b = b1->clone();
 	a->invert();
-	a->clipTo(b);
-	b->clipTo(a);
+	a->clip_to(b);
+	b->clip_to(a);
 	b->invert();
-	b->clipTo(a);
+	b->clip_to(a);
 	b->invert();
-	a->build(b->allPolygons());
+	a->build(b->all_polygons());
 	a->invert();
-	csgnode * ret = new csgnode(a->allPolygons());
+	Node * ret = new Node(a->all_polygons());
 	delete a; a = 0;
 	delete b; b = 0;
 	return ret;
@@ -243,29 +243,29 @@ inline static csgnode * csg_subtract(const csgnode * a1, const csgnode * b1)
 
 // Return a new CSG solid representing space both this solid and in the
 // solid `csg`. Neither this solid nor the solid `csg` are modified.
-inline static csgnode * csg_intersect(const csgnode * a1, const csgnode * b1)
+inline static Node * csg_intersect(const Node * a1, const Node * b1)
 {
-	csgnode * a = a1->clone();
-	csgnode * b = b1->clone();
+	Node * a = a1->clone();
+	Node * b = b1->clone();
 	a->invert();
-	b->clipTo(a);
+	b->clip_to(a);
 	b->invert();
-	a->clipTo(b);
-	b->clipTo(a);
-	a->build(b->allPolygons());
+	a->clip_to(b);
+	b->clip_to(a);
+	a->build(b->all_polygons());
 	a->invert();
-	csgnode * ret = new csgnode(a->allPolygons());
+	Node * ret = new Node(a->all_polygons());
 	delete a; a = 0;
 	delete b; b = 0;
 	return ret;
 }
 
 // Convert solid space to empty space and empty space to solid space.
-void csgnode::invert()
+void Node::invert()
 {	
 	for (size_t i = 0; i < this->polygons.size(); i++)
 		this->polygons[i].flip();
-	this->_plane.flip();
+	this->plane.flip();
 	if (this->front) this->front->invert();
 	if (this->back) this->back->invert();
 	std::swap(this->front, this->back);
@@ -273,16 +273,16 @@ void csgnode::invert()
 
 // Recursively remove all polygons in `polygons` that are inside this BSP
 // tree.
-std::vector<polygon> csgnode::clipPolygons(const std::vector<polygon> & list) const
+std::vector<Polygon> Node::clip_polygons(const std::vector<Polygon> & list) const
 {
-	if (!this->_plane.ok()) return list;
-	std::vector<polygon> list_front, list_back;	
+	if (!this->plane.ok()) return list;
+	std::vector<Polygon> list_front, list_back;	
 	for (size_t i = 0; i < list.size(); i++)
 	{
-		this->_plane.splitPolygon(list[i], list_front, list_back, list_front, list_back);
+		this->plane.split_polygon(list[i], list_front, list_back, list_front, list_back);
 	}
-	if (this->front) list_front = this->front->clipPolygons(list_front);
-	if (this->back) list_back = this->back->clipPolygons(list_back);
+	if (this->front) list_front = this->front->clip_polygons(list_front);
+	if (this->back) list_back = this->back->clip_polygons(list_back);
 	else list_back.clear();
 	
 	list_front.insert(list_front.end(), list_back.begin(), list_back.end());
@@ -291,30 +291,30 @@ std::vector<polygon> csgnode::clipPolygons(const std::vector<polygon> & list) co
 
 // Remove all polygons in this BSP tree that are inside the other BSP tree
 // `bsp`.
-void csgnode::clipTo(const csgnode * other)
+void Node::clip_to(const Node * other)
 {
-	this->polygons = other->clipPolygons(this->polygons);
-	if (this->front) this->front->clipTo(other);
-	if (this->back) this->back->clipTo(other);
+	this->polygons = other->clip_polygons(this->polygons);
+	if (this->front) this->front->clip_to(other);
+	if (this->back) this->back->clip_to(other);
 }
 
 // Return a list of all polygons in this BSP tree.
-std::vector<polygon> csgnode::allPolygons() const
+std::vector<Polygon> Node::all_polygons() const
 {
-	std::vector<polygon> list = this->polygons;
-	std::vector<polygon> list_front, list_back;
-	if (this->front) list_front = this->front->allPolygons();
-	if (this->back) list_back = this->back->allPolygons();
+	std::vector<Polygon> list = this->polygons;
+	std::vector<Polygon> list_front, list_back;
+	if (this->front) list_front = this->front->all_polygons();
+	if (this->back) list_back = this->back->all_polygons();
 	list.insert(list.end(), list_front.begin(), list_front.end());
 	list.insert(list.end(), list_back.begin(), list_back.end());
 	return list;
 }
 
-csgnode * csgnode::clone() const
+Node * Node::clone() const
 {
-	csgnode * ret = new csgnode();
+	Node * ret = new Node();
 	ret->polygons = this->polygons;
-	ret->_plane = this->_plane;
+	ret->plane = this->plane;
 	if (this->front) ret->front = this->front->clone();
 	if (this->back) ret->back = this->back->clone();
 	return ret;
@@ -324,37 +324,37 @@ csgnode * csgnode::clone() const
 // new polygons are filtered down to the bottom of the tree and become new
 // nodes there. Each set of polygons is partitioned using the first polygon
 // (no heuristic is used to pick a good split).
-void csgnode::build(const std::vector<polygon> & list)
+void Node::build(const std::vector<Polygon> & list)
 {
 	if (!list.size()) return;
-	if (!this->_plane.ok()) this->_plane = list[0]._plane;
-	std::vector<polygon> list_front, list_back;
+	if (!this->plane.ok()) this->plane = list[0] .plane;
+	std::vector<Polygon> list_front, list_back;
 	for (size_t i = 0; i < list.size(); i++) 
 	{
-		this->_plane.splitPolygon(list[i], this->polygons, this->polygons, list_front, list_back);
+		this->plane.split_polygon(list[i], this->polygons, this->polygons, list_front, list_back);
 	}
 	if (list_front.size()) 
 	{
-		if (!this->front) this->front = new csgnode;
+		if (!this->front) this->front = new Node;
 		this->front->build(list_front);
 	}
 	if (list_back.size()) 
 	{
-		if (!this->back) this->back = new csgnode;
+		if (!this->back) this->back = new Node;
 		this->back->build(list_back);
 	}
 }
 
-csgnode::csgnode() : front(0), back(0)
+Node::Node() : front(0), back(0)
 {
 }
 
-csgnode::csgnode(const std::vector<polygon> & list) : front(0), back(0)
+Node::Node(const std::vector<Polygon> & list) : front(0), back(0)
 {
 	build(list);
 }
 
-csgnode::~csgnode()
+Node::~Node()
 {
 	delete front;
 	delete back;
@@ -362,64 +362,64 @@ csgnode::~csgnode()
 
 // Public interface implementation
 
-inline static std::vector<polygon> modelToPolygons(const model & model)
+inline static std::vector<Polygon> model_to_polygons(const Model & model)
 {
-	std::vector<polygon> list;
+	std::vector<Polygon> list;
 	for (size_t i = 0; i < model.indices.size(); i+= 3)
 	{
-		std::vector<vertex> triangle;
+		std::vector<Vertex> triangle;
 		for (int j = 0; j < 3; j++)
 		{
-			vertex v = model.vertices[model.indices[i + j]];
+			Vertex v = model.vertices[model.indices[i + j]];
 			triangle.push_back(v);
 		}
-		list.push_back(polygon(triangle));
+		list.push_back(Polygon(triangle));
 	}
 	return list;
 }
 
-inline static model modelFromPolygons(const std::vector<polygon> & polygons)
+inline static Model model_from_polygons(const std::vector<Polygon> & polygons)
 {
-	model model;
+	Model model;
 	int p = 0;
 	for (size_t i = 0; i < polygons.size(); i++)
 	{
-		const polygon & poly = polygons[i];
+		const Polygon & poly = polygons[i];
 		for (size_t j = 2; j < poly.vertices.size(); j++)
 		{
 			model.vertices.push_back(poly.vertices[0]);		model.indices.push_back(p++);
-			model.vertices.push_back(poly.vertices[j - 1]);	model.indices.push_back(p++);
+			model.vertices.push_back(poly.vertices[j - 1]);		model.indices.push_back(p++);
 			model.vertices.push_back(poly.vertices[j]);		model.indices.push_back(p++);			
 		}
 	}
 	return model;
 }
 
-typedef csgnode * csg_function(const csgnode * a1, const csgnode * b1);
+typedef Node * csg_function(const Node * a1, const Node * b1);
 
-inline static model operation(const model & a, const model & b, csg_function fun)
+inline static Model operation(const Model & a, const Model & b, csg_function fun)
 {
-	csgnode * A = new csgnode(modelToPolygons(a));
-	csgnode * B = new csgnode(modelToPolygons(b));
-	csgnode * AB = fun(A, B);
-	std::vector<polygon> polygons = AB->allPolygons();
+	Node * A = new Node(model_to_polygons(a));
+	Node * B = new Node(model_to_polygons(b));
+	Node * AB = fun(A, B);
+	std::vector<Polygon> polygons = AB->all_polygons();
 	delete A; A = 0;
 	delete B; B = 0;
 	delete AB; AB = 0;
-	return modelFromPolygons(polygons);
+	return model_from_polygons(polygons);
 }
 
-model get_union(const model & a, const model & b)
+Model get_union(const Model & a, const Model & b)
 {
 	return operation(a, b, csg_union);
 }
 
-model get_intersection(const model & a, const model & b)
+Model get_intersection(const Model & a, const Model & b)
 {
 	return operation(a, b, csg_intersect);
 }
 
-model get_difference(const model & a, const model & b)
+Model get_difference(const Model & a, const Model & b)
 {
 	return operation(a, b, csg_subtract);
 }
